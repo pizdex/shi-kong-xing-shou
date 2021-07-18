1e0b30cf16cd438450b3c4f31a792af87edbf9bb
Zumi Daxuya <daxuya.zumi+code@protonmail.com>
2021-07-18 19:57:47 +0700

    test out dumping events

--- banks/bank_07.asm
+++ banks/bank_07.asm
@@ -2,7 +2,18 @@ unk_007_4000::
 	dr $1c000, $1c30f
 
 unk_007_430f::
-	dr $1c30f, $1cf00
+	dr $1c30f, $1ce21
+
+unk_007_4e21::
+	warp $00, ABSOLUTE, $01, $02, $00
+	signpost $08, $08, 4
+	db -1
+
+unk_007_4e2e::
+	db -1
+
+unk_007_4e2f::
+	dr $1ce2f, $1cf00
 
 
 SECTION "banknum7", ROMX[$7fff], BANK[$7]
